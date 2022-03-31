cdf98b589d3e8173b909fca66d8672504d3b384f
zhaowei129 <zhaowei129@ke.com>
2022-03-31 12:04:07 +0800

    fix examples.ponaRulerPlugin

--- examples/src/10.PanoRulerPlugin/App.tsx
+++ examples/src/10.PanoRulerPlugin/App.tsx
@@ -4,6 +4,7 @@ import React, { FC } from "react";
 import { useWindowDimensions } from "./useWindowDimensions";
 import { work } from './mockData'
 import PanoRulerPluginUsage from "./PanoRulerPluginUsage";
+import { parseWork } from "@realsee/five";
 
 const FiveProvider = createFiveProvider({
   onlyRenderIfNeeds: true,
@@ -15,7 +16,7 @@ const FiveProvider = createFiveProvider({
 const App: FC = () => {
   const size = useWindowDimensions();
 
-  return work && <FiveProvider initialWork={work} ref={ref => Object.assign(window, {$five: ref?.five})}>
+  return work && <FiveProvider initialWork={parseWork(work)} ref={ref => Object.assign(window, {$five: ref?.five})}>
     <FiveCanvas {...size}/>
     <PanoRulerPluginUsage/>
   </FiveProvider>;
--- examples/src/10.PanoRulerPlugin/PanoRulerPluginUsage.tsx
+++ examples/src/10.PanoRulerPlugin/PanoRulerPluginUsage.tsx
@@ -1,3 +1,4 @@
+import { Box, Paper, ButtonGroup, Button } from '@mui/material'
 import { PanoRulerPlugin } from '@realsee/dnalogel'
 import { unsafe__useFiveInstance, useFiveEventCallback } from '@realsee/five/react'
 import * as React from 'react'
@@ -7,29 +8,39 @@ import { roomInfo, roomRules } from './mockData'
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface PanoRulerPluginUsePropTypes {}
 
+const OPERATE_OPTIONS = {
+  OPEN: '开启标尺',
+  CLOSE: '关闭标尺',
+}
 
-
-const PanoRulerPluginUsage=(props: PanoRulerPluginUsePropTypes)=> {
+const PanoRulerPluginUsage = (props: PanoRulerPluginUsePropTypes) => {
   const five = unsafe__useFiveInstance()
   const panoRulerPlugin = five.plugins.panoRulerPlugin as ReturnType<typeof PanoRulerPlugin>
   // 标尺状态
   const [state, setState] = React.useState(panoRulerPlugin.state.enable)
   useFiveEventCallback('modelLoaded', async () => {
-    await panoRulerPlugin.load(roomInfo, roomRules)
+    await panoRulerPlugin.load(roomInfo, roomRules, {
+      distanceText: (distance) => `约${distance.toFixed(1)}米`,
+    })
     setState(panoRulerPlugin.state.enable)
   })
 
   return (
-    <button
-        style={{ position: 'absolute', right: '36px', top: '24px',background: '#0078e7', color: '#fff', padding:'7px 14px', border:'none', borderRadius:'2px'  }}
-        onClick={() => {
-          panoRulerPlugin[panoRulerPlugin.state.enable ? 'disable' : 'enable']()
-          setState(panoRulerPlugin.state.enable)
-        }}
-      >
-        {state ? '关闭标尺' : '开启标尺'}
-      </button>
+    <Box>
+      <Paper sx={{ position: 'fixed', top: '10px', right: '10px', backgroundColor: 'transparent' }}>
+        <ButtonGroup size="large" aria-label="large button group" orientation="vertical">
+          <Button
+            onClick={() => {
+              panoRulerPlugin[panoRulerPlugin.state.enable ? 'disable' : 'enable']()
+              setState(panoRulerPlugin.state.enable)
+            }}
+          >
+            {state ? OPERATE_OPTIONS.CLOSE : OPERATE_OPTIONS.OPEN}
+          </Button>
+        </ButtonGroup>
+      </Paper>
+    </Box>
   )
 }
 
-export default PanoRulerPluginUsage;
+export default PanoRulerPluginUsage
