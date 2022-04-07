7b324528e19c00b6952a2f53abae3700111265ea
Angry-Sparrow <beqan2016@163.com>
2022-04-07 16:00:37 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   // root: './src',
-  // base: '/',
+  base: '/dnalogel/',
   plugins: [react(), svelte()],
   build: {
     // assetsDir: '../assets',
@@ -15,15 +15,17 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, './index.html'),
-        // ModelViewPlugin: resolve(__dirname, 'src/0.ModelViewPlugin/index.html'),
-        // PanoFloorplanRadarPlugin: resolve(__dirname, 'src/1.PanoFloorplanRadarPlugin/index.html'),
-        // ModelRoomLabelPlugin: resolve(__dirname, 'src/2.ModelRoomLabelPlugin/index.html'),
-        // TopviewFloorplanPlugin: resolve(__dirname, 'src/3.TopviewFloorplanPlugin/index.html'),
-        // ModelChassisCompassPlugin: resolve(__dirname, 'src/4.ModelChassisCompassPlugin/index.html'),
-        // ModelEntryDoorGuidePlugin: resolve(__dirname, 'src/5.ModelEntryDoorGuidePlugin/index.html'),
-        // CSS3DRenderPlugin: resolve(__dirname, 'src/6.CSS3DRenderPlugin/index.html'),
-        // ModelEntryDoorGuCameraMovementPluginidePlugin: resolve(__dirname, 'src/7.CameraMovementPlugin/index.html'),
-        // PanoCursorRaycasterPlugin: resolve(__dirname, 'src/8.PanoCursorRaycasterPlugin/index.html'),
+        ModelViewPlugin: resolve(__dirname, 'src/0.ModelViewPlugin/index.html'),
+        PanoFloorplanRadarPlugin: resolve(__dirname, 'src/1.PanoFloorplanRadarPlugin/index.html'),
+        ModelRoomLabelPlugin: resolve(__dirname, 'src/2.ModelRoomLabelPlugin/index.html'),
+        TopviewFloorplanPlugin: resolve(__dirname, 'src/3.TopviewFloorplanPlugin/index.html'),
+        ModelChassisCompassPlugin: resolve(__dirname, 'src/4.ModelChassisCompassPlugin/index.html'),
+        ModelEntryDoorGuidePlugin: resolve(__dirname, 'src/5.ModelEntryDoorGuidePlugin/index.html'),
+        CSS3DRenderPlugin: resolve(__dirname, 'src/6.CSS3DRenderPlugin/index.html'),
+        ModelEntryDoorGuCameraMovementPluginidePlugin: resolve(__dirname, 'src/7.CameraMovementPlugin/index.html'),
+        ModelFloorplanPlugin: resolve(__dirname, 'src/8.ModelFloorplanPlugin/index.html'),
+        PanoCompassPlugin: resolve(__dirname, 'src/9.PanoCompassPlugin/index.html'),
+        PanoRulerPlugin: resolve(__dirname, 'src/10.PanoRulerPlugin/index.html'),
       },
       output: {
         chunkFileNames: 'static/js/[name]-[hash].js',
