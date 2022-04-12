029609666230553888d5ae7aff7c4404fcf9ed46
Angry-Sparrow <beqan2016@163.com>
2022-04-12 12:03:56 +0800

    feat: perfect PanoMeasurePlugin example

--- examples/src/PanoMeasurePlugin/PanoMeasurePluginUsage.tsx
+++ examples/src/PanoMeasurePlugin/PanoMeasurePluginUsage.tsx
@@ -10,9 +10,27 @@ interface PanoRulerPluginUsePropTypes {
 const PanoMeasurePluginUsage = (props: PanoRulerPluginUsePropTypes) => {
     const five = unsafe__useFiveInstance()
     const panoMeasurePlugin = five.plugins.panoMeasurePlugin as ReturnType<typeof PanoMeasurePlugin>
-    const [measureEnableBtn, setMeasureEnableBtn] = React.useState(false)
+    const [measureEnableBtn, setMeasureEnableBtn] = React.useState<boolean>(false)
+
+    const handlePanoMeasurePluginListener = () => {
+        panoMeasurePlugin.hook.on("modeChange", (mode) => {
+            console.log("__mode__", mode)
+        })
+
+        panoMeasurePlugin.hook.on("enable", () => {
+            console.log('开启测量工具')
+            setMeasureEnableBtn(false)
+        })
+
+        panoMeasurePlugin.hook.on("disable", () => {
+            console.log('关闭测量工具')
+            setMeasureEnableBtn(true)
+        })
+    }
 
     useEffect(() => {
+        handlePanoMeasurePluginListener()
+
         const container = document.querySelector('.plugin-full-screen-container') as HTMLElement
         if (container) {
             panoMeasurePlugin.appendTo(container)
@@ -21,17 +39,6 @@ const PanoMeasurePluginUsage = (props: PanoRulerPluginUsePropTypes) => {
 
     useFiveEventCallback('modelLoaded', async () => {
         panoMeasurePlugin.enable()
-        panoMeasurePlugin.hook.on("modeChange", (mode) => {
-            console.log("__mode__", mode)
-        })
-
-        panoMeasurePlugin.hook.on("enable", () => {
-            console.log('开启测量工具')
-        })
-
-        panoMeasurePlugin.hook.on("disable", () => {
-            console.log('关闭测量工具')
-        })
     })
 
     const handleMeasureEnable = () => {
