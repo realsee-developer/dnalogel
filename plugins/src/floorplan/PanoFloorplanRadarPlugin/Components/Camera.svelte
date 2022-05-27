7365170cd4bd2d7975ff8f2eab28aba359fb05fa
jiasonglin001 <jiasonglin001@ke.com>
2022-05-27 18:07:43 +0800

    fix(floorplan-radar-plugin): 修复 reload 户型图数据雷达图标位置没有适配

--- plugins/src/floorplan/PanoFloorplanRadarPlugin/Components/Camera.svelte
+++ plugins/src/floorplan/PanoFloorplanRadarPlugin/Components/Camera.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import type { Five } from '@realsee/five'
+  import type { Five, Pose } from '@realsee/five'
   import type { FloorplanData } from '../../typings/floorplanData'
   import { onMount, onDestroy } from 'svelte'
   import { CAMERA_IMAGE } from '../../Assets/camera'
@@ -10,43 +10,48 @@
   export let floorplanData: FloorplanData
   export let cameraImageUrl: undefined | string
 
+  let panoIndex = five.getCurrentState().panoIndex
+  let longitude = five.getCurrentState().longitude
+
+  // 初始状态，不能等 mount 之后再计算，那样第一次 camera 展示的时候也会有动画
+  $: positionTransform = getCameraPositionTransform(floorplanData, panoIndex)
+  $: rotateTransform = getCameraRotateTransform(longitude)
+
   /** 获取相机在当前 Five 状态下的位移 */
-  function getCameraPositionTransform(panoIndex: number) {
-    const observer = floorplanData.observers[panoIndex]
+  function getCameraPositionTransform(floorplanData: FloorplanData, panoIndex: number) {
     const bounding = floorplanData.bounding
+    const floorplanObserver = floorplanData.observers[panoIndex]
+    if (!floorplanObserver) return ''
     const clientWidth = (bounding.max.x - bounding.min.x) * pxmm
     const clientHeight = (bounding.max.y - bounding.min.y) * pxmm
-    if (!observer) return ''
-    const left = Math.floor(observer.positionInImage.x * clientWidth)
-    const top = Math.floor(observer.positionInImage.y * clientHeight)
+    const left = Math.floor(floorplanObserver.positionInImage.x * clientWidth)
+    const top = Math.floor(floorplanObserver.positionInImage.y * clientHeight)
     return `translate(${left}px, ${top}px)`
   }
+
   /** 获取相机在当前 Five 状态下的旋转 */
-  function getCameraRotationTransform() {
-    const { longitude } = five.getCurrentState()
+  function getCameraRotateTransform(longitude: number) {
     // 为什么要加 45 ==> 因为 UI 给的图初始角度是 -45
     const rotate = Math.floor((longitude / Math.PI) * 180) * -1 + 45
     return `rotate(${rotate}deg)`
   }
-  const changePosition = (panoIndex: number) => {
-    positionTransform = getCameraPositionTransform(panoIndex)
+
+  function onFivePanoWillArrive(_panoIndex: number) {
+    panoIndex = _panoIndex
   }
-  const changeRotate = throttle(() => {
-    rotateTransform = getCameraRotationTransform()
+
+  const onFiveCameraDirectionUpdate = throttle((pose: Pose) => {
+    longitude = pose.longitude
   }, 1000 / 60)
 
-  // 初始状态，不能等 mount 之后再计算，那样第一次 camera 展示的时候也会有动画
-  let rotateTransform = getCameraRotationTransform()
-  let positionTransform = getCameraPositionTransform(five.getCurrentState().panoIndex)
-
   onMount(() => {
-    five.on('panoWillArrive', changePosition)
-    five.on('cameraDirectionUpdate', changeRotate)
+    five.on('panoWillArrive', onFivePanoWillArrive)
+    five.on('cameraDirectionUpdate', onFiveCameraDirectionUpdate)
   })
 
   onDestroy(() => {
-    five.off('panoWillArrive', changePosition)
-    five.off('cameraDirectionUpdate', changeRotate)
+    five.off('panoWillArrive', onFivePanoWillArrive)
+    five.off('cameraDirectionUpdate', onFiveCameraDirectionUpdate)
   })
 </script>
 
