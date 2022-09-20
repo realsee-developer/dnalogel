c0447610112ff50d34b33fac9b9f05ccdc196fc1
jiasonglin001 <jiasonglin001@ke.com>
2022-09-20 15:54:41 +0800

    chore(*__example): fix lint

--- src/ModelFloorplanPlugin/App.tsx
+++ src/ModelFloorplanPlugin/App.tsx
@@ -1,48 +1,62 @@
-import { FLOOR_PLAN_ATTACHED_TO, ModelFloorplanPlugin } from "@realsee/dnalogel";
-import { createFiveProvider, FiveCanvas } from "@realsee/five/react";
-import { parseWork } from "@realsee/five";
-import * as React from "react";
-import { useWindowDimensions } from "./useWindowDimensions";
-import ModelFloorplanPluginUse from "./ModelFloorplanPluginUse";
+import { FLOOR_PLAN_ATTACHED_TO, ModelFloorplanPlugin } from '@realsee/dnalogel'
+import { createFiveProvider, FiveCanvas } from '@realsee/five/react'
+import { parseWork } from '@realsee/five'
+import * as React from 'react'
+import { useWindowDimensions } from './useWindowDimensions'
+import ModelFloorplanPluginUse from './ModelFloorplanPluginUse'
 import { Box } from '@mui/material'
-import getInitialParamFromUrl from "../utils/getInitialParamFromUrl";
-import useFetchDatas, { DATA_TYPES } from "../utils/useFetchDatas";
+import getInitialParamFromUrl from '../utils/getInitialParamFromUrl'
+import useFetchDatas, { DATA_TYPES } from '../utils/useFetchDatas'
 
 const defaultPluginParam = {
-    attachedTo: FLOOR_PLAN_ATTACHED_TO.CEILING // 户型图吸附至天花板，默认吸附至几何中心
+  attachedTo: FLOOR_PLAN_ATTACHED_TO.CEILING, // 户型图吸附至天花板，默认吸附至几何中心
 }
 
 const initialParamFromUrl = getInitialParamFromUrl()
 
-const pluginParams = (JSON.stringify(initialParamFromUrl) !== '{}') ? initialParamFromUrl : defaultPluginParam
+const pluginParams =
+  JSON.stringify(initialParamFromUrl) !== '{}' ? initialParamFromUrl : defaultPluginParam
 
 const FiveProvider = createFiveProvider({
-    imageOptions: { size: 512 }, // 图片默认分辨率
-    textureOptions: { size: 512 }, // 贴图默认分辨率
-    plugins: [
-        [
-            ModelFloorplanPlugin,
-            'modelFloorplanPlugin',
-            {
-                selector: '.plugin-full-screen-container',
-                ...pluginParams
-            }
-
-        ]
-    ]
-});
+  imageOptions: { size: 512 }, // 图片默认分辨率
+  textureOptions: { size: 512 }, // 贴图默认分辨率
+  plugins: [
+    [
+      ModelFloorplanPlugin,
+      'modelFloorplanPlugin',
+      {
+        selector: '.plugin-full-screen-container',
+        ...pluginParams,
+      },
+    ],
+  ],
+})
 
 const App: React.FC = () => {
-    const size = useWindowDimensions();
-    const work = useFetchDatas(DATA_TYPES.WORK)
-    return work && <FiveProvider initialWork={parseWork(work)} ref={ref => Object.assign(window, { $five: ref?.five })}>
-		<FiveCanvas {...size} />
-	    <Box
-		    className="plugin-full-screen-container"
-		    sx={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', pointerEvents: 'none' }}
-	    />
-		<ModelFloorplanPluginUse />
-	</FiveProvider>;
-};
+  const size = useWindowDimensions()
+  const work = useFetchDatas(DATA_TYPES.WORK)
+  return (
+    work && (
+      <FiveProvider
+        initialWork={parseWork(work)}
+        ref={(ref) => Object.assign(window, { $five: ref?.five })}
+      >
+        <FiveCanvas {...size} />
+        <Box
+          className="plugin-full-screen-container"
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            pointerEvents: 'none',
+          }}
+        />
+        <ModelFloorplanPluginUse />
+      </FiveProvider>
+    )
+  )
+}
 
-export default App;
+export default App
--- src/ModelFloorplanPlugin/ModelFloorplanPluginUse.tsx
+++ src/ModelFloorplanPlugin/ModelFloorplanPluginUse.tsx
@@ -1,66 +1,81 @@
-import * as React from 'react';
-import { Five, Mode } from "@realsee/five"
+import * as React from 'react'
+import { Five, Mode } from '@realsee/five'
 import {
-    unsafe__useFiveInstance,
-    useFiveEventCallback,
-    useFiveModelReadyState,
-    useFiveState
-} from "@realsee/five/react";
+  unsafe__useFiveInstance,
+  useFiveEventCallback,
+  useFiveModelReadyState,
+  useFiveState,
+} from '@realsee/five/react'
 import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material'
-import DirectionsWalkIcon from "@mui/icons-material/DirectionsWalk";
-import ViewInArIcon from "@mui/icons-material/ViewInAr";
-import useFetchDatas, { DATA_TYPES } from "../utils/useFetchDatas";
+import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk'
+import ViewInArIcon from '@mui/icons-material/ViewInAr'
+import useFetchDatas, { DATA_TYPES } from '../utils/useFetchDatas'
 
 interface ModelFloorplanPluginUsePropTypes {}
 
 const ModelFloorplanPluginUse = (props: ModelFloorplanPluginUsePropTypes) => {
-    const five = unsafe__useFiveInstance()
-    const [fiveState, setFiveState] = useFiveState()
-    const fiveModelReadyState = useFiveModelReadyState()
-    const floorplanServerData = useFetchDatas(DATA_TYPES.FLOOR_PLAN_SERVER_PLUGIN_DATA)
+  const five = unsafe__useFiveInstance()
+  const [fiveState, setFiveState] = useFiveState()
+  const fiveModelReadyState = useFiveModelReadyState()
+  const floorplanServerData = useFetchDatas(DATA_TYPES.FLOOR_PLAN_SERVER_PLUGIN_DATA)
 
-    // 插件事件监听
-    React.useEffect(() => {
-        five.plugins.modelFloorplanPlugin.hooks.on('showAnimationEnded', () => {
-            console.log('🐶-- ModelFloorplanPlugin -- show')
-        })
-        five.plugins.modelFloorplanPlugin.hooks.on('hide', () => {
-            console.log('🐶-- ModelFloorplanPlugin -- hide')
-        })
-    }, [five])
+  // 插件事件监听
+  React.useEffect(() => {
+    five.plugins.modelFloorplanPlugin.hooks.on('showAnimationEnded', () => {
+      console.log('🐶-- ModelFloorplanPlugin -- show')
+    })
+    five.plugins.modelFloorplanPlugin.hooks.on('hide', () => {
+      console.log('🐶-- ModelFloorplanPlugin -- hide')
+    })
+  }, [five])
 
-    useFiveEventCallback("modelLoaded", () => {
-        if(!floorplanServerData || JSON.stringify(floorplanServerData) === '{}') return
-        Promise.resolve(five.plugins.modelFloorplanPlugin.load(floorplanServerData)).then(() => {
-            five.plugins.modelFloorplanPlugin.show()
-        })
-    }, [floorplanServerData])
+  useFiveEventCallback(
+    'modelLoaded',
+    () => {
+      if (!floorplanServerData || JSON.stringify(floorplanServerData) === '{}') return
+      Promise.resolve(five.plugins.modelFloorplanPlugin.load(floorplanServerData)).then(() => {
+        five.plugins.modelFloorplanPlugin.show()
+      })
+    },
+    [floorplanServerData],
+  )
 
-    useFiveEventCallback("initAnimationEnded", () => {
-        if (fiveState.mode === Five.Mode.Floorplan) {
-            five.plugins.modelFloorplanPlugin.show()
-        }
-    }, [fiveState.mode])
+  useFiveEventCallback(
+    'initAnimationEnded',
+    () => {
+      if (fiveState.mode === Five.Mode.Floorplan) {
+        five.plugins.modelFloorplanPlugin.show()
+      }
+    },
+    [fiveState.mode],
+  )
 
-    if (fiveModelReadyState !== 'Loaded') return null
-    return (
-        <Paper
-            sx={{ position: "fixed", bottom: 0, left: 0, right: 0, backgroundColor: 'transparent' }}
-            className="topview-floorplan-plugin-use"
-        >
-            <BottomNavigation
-                showLabels
-                value={fiveState.mode}
-                onChange={(_, newValue: Mode) => {
-                    setFiveState({ mode: newValue });
-                }}
-            >
-                <BottomNavigationAction label="全景漫游" icon={<DirectionsWalkIcon/>} value={Five.Mode.Panorama}/>
-                <BottomNavigationAction label="空间总览" icon={<ViewInArIcon/>} value={Five.Mode.Floorplan}/>
-            </BottomNavigation>
-        </Paper>
-    )
+  if (fiveModelReadyState !== 'Loaded') return null
+  return (
+    <Paper
+      sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, backgroundColor: 'transparent' }}
+      className="topview-floorplan-plugin-use"
+    >
+      <BottomNavigation
+        showLabels
+        value={fiveState.mode}
+        onChange={(_, newValue: Mode) => {
+          setFiveState({ mode: newValue })
+        }}
+      >
+        <BottomNavigationAction
+          label="全景漫游"
+          icon={<DirectionsWalkIcon />}
+          value={Five.Mode.Panorama}
+        />
+        <BottomNavigationAction
+          label="空间总览"
+          icon={<ViewInArIcon />}
+          value={Five.Mode.Floorplan}
+        />
+      </BottomNavigation>
+    </Paper>
+  )
+}
 
-};
-
-export default ModelFloorplanPluginUse;
+export default ModelFloorplanPluginUse
