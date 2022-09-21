2fa2ca140f1c0337de88ef52ee0c78a00bd14d5a
jiasonglin001 <jiasonglin001@ke.com>
2022-09-21 15:42:31 +0800

    feat(pano-ruler__example): panoRulerPlugin 添加单位转换示例

--- src/PanoRulerPlugin/App.tsx
+++ src/PanoRulerPlugin/App.tsx
@@ -1,28 +1,33 @@
-import { PanoRulerPlugin } from "@realsee/dnalogel";
-import { createFiveProvider, FiveCanvas } from "@realsee/five/react";
-import * as React from "react";
-import { useWindowDimensions } from "./useWindowDimensions";
-import PanoRulerPluginUsage from "./PanoRulerPluginUsage";
-import { parseWork } from "@realsee/five";
-import useFetchDatas, { DATA_TYPES } from "../utils/useFetchDatas";
+import { PanoRulerPlugin } from '@realsee/dnalogel/libs/panoRulerPlugin'
+import { createFiveProvider, FiveCanvas } from '@realsee/five/react'
+import * as React from 'react'
+import { useWindowDimensions } from './useWindowDimensions'
+import PanoRulerPluginUsage from './PanoRulerPluginUsage'
+import { parseWork } from '@realsee/five'
+import useFetchDatas, { DATA_TYPES } from '../utils/useFetchDatas'
 
 const FiveProvider = createFiveProvider({
-    imageOptions: { size: 512 }, // 图片默认分辨率
-    textureOptions: { size: 512 }, // 贴图默认分辨率
-    onlyRenderIfNeeds: true,
-    plugins: [
-        [PanoRulerPlugin, 'panoRulerPlugin', {}]
-    ]
-});
+  imageOptions: { size: 512 }, // 图片默认分辨率
+  textureOptions: { size: 512 }, // 贴图默认分辨率
+  onlyRenderIfNeeds: true,
+  plugins: [[PanoRulerPlugin, 'panoRulerPlugin', {}]],
+})
 
 const App: React.FC = () => {
-    const size = useWindowDimensions();
-    const work = useFetchDatas(DATA_TYPES.WORK, 'pWLy9nekmQdMXqja')
+  const size = useWindowDimensions()
+  const work = useFetchDatas(DATA_TYPES.WORK, 'pWLy9nekmQdMXqja')
 
-    return work && <FiveProvider initialWork={parseWork(work)} ref={ref => Object.assign(window, { $five: ref?.five })}>
-		<FiveCanvas {...size} />
-		<PanoRulerPluginUsage />
-	</FiveProvider>;
-};
+  return (
+    work && (
+      <FiveProvider
+        initialWork={parseWork(work)}
+        ref={(ref) => Object.assign(window, { $five: ref?.five })}
+      >
+        <FiveCanvas {...size} />
+        <PanoRulerPluginUsage />
+      </FiveProvider>
+    )
+  )
+}
 
-export default App;
+export default App
--- src/PanoRulerPlugin/PanoRulerPluginUsage.tsx
+++ src/PanoRulerPlugin/PanoRulerPluginUsage.tsx
@@ -1,52 +1,93 @@
 import { Box, Button, Paper } from '@mui/material'
-import { PanoRulerPlugin } from '@realsee/dnalogel'
-import { unsafe__useFiveInstance, useFiveEventCallback, useFiveModelReadyState } from '@realsee/five/react'
+import { PanoRulerPlugin } from '@realsee/dnalogel/libs/PanoRulerPlugin'
+import {
+  unsafe__useFiveInstance,
+  useFiveEventCallback,
+  useFiveModelReadyState,
+} from '@realsee/five/react'
 import * as React from 'react'
-import useFetchDatas, { DATA_TYPES } from "../utils/useFetchDatas";
-
-interface PanoRulerPluginUsePropTypes {
-}
+import useFetchDatas, { DATA_TYPES } from '../utils/useFetchDatas'
 
 enum OPERATE_OPTIONS {
-    OPEN = '开启标尺',
-    CLOSE = '关闭标尺'
+  OPEN = '开启标尺',
+  CLOSE = '关闭标尺',
 }
 
-const PanoRulerPluginUsage = (props: PanoRulerPluginUsePropTypes) => {
-    const fiveModelReadyState = useFiveModelReadyState()
-    const five = unsafe__useFiveInstance()
-    const panoRulerPlugin = five.plugins.panoRulerPlugin as ReturnType<typeof PanoRulerPlugin>
-    const panoRulerData = useFetchDatas(DATA_TYPES.PANO_RULER_PLUGIN_SERVER_DATA, 'pWLy9nekmQdMXqja')
+const PanoRulerPluginUsage = () => {
+  const fiveModelReadyState = useFiveModelReadyState()
+  const five = unsafe__useFiveInstance()
+  const panoRulerPlugin = five.plugins.panoRulerPlugin as ReturnType<typeof PanoRulerPlugin>
+  const panoRulerData = useFetchDatas(
+    DATA_TYPES.PANO_RULER_PLUGIN_SERVER_DATA,
+    'pWLy9nekmQdMXqja',
+  ) as any
+  // 标尺状态
+  const [rulerEnable, setRulerEnable] = React.useState(panoRulerPlugin.state.enable)
 
-    // 标尺状态
-    const [rulerEnable, setRulerEnable] = React.useState(panoRulerPlugin.state.enable)
+  const [isDefaultUnit, setIsDefaultUnit] = React.useState(true)
 
-    useFiveEventCallback('modelLoaded', async () => {
-        if (!panoRulerData.pano_ruler_data.roomInfo || !panoRulerData.pano_ruler_data.roomRules) return
+  function toggleUnit() {
+    setIsDefaultUnit(!isDefaultUnit)
+  }
 
-        await panoRulerPlugin.load(panoRulerData.pano_ruler_data.roomInfo, panoRulerData.pano_ruler_data.roomRules, {
-            distanceText: (distance) => `约 ${distance.toFixed(1)}米`,
-        })
+  React.useEffect(() => {
+    panoRulerPlugin.changeConfigs({
+      distanceText(distance) {
+        return isDefaultUnit
+          ? distance.toFixed(1).toString() + 'm'
+          : (distance * 3.2808).toFixed(1) + 'ft'
+      },
+    })
+  }, [isDefaultUnit])
 
-        panoRulerPlugin.enable()
-        setRulerEnable(panoRulerPlugin.state.enable)
-    }, [panoRulerData])
+  useFiveEventCallback(
+    'modelLoaded',
+    async () => {
+      if (!panoRulerData.pano_ruler_data.roomInfo || !panoRulerData.pano_ruler_data.roomRules)
+        return
 
-    const handleRulerEnable = () => {
-        panoRulerPlugin[panoRulerPlugin.state.enable ? 'disable' : 'enable']()
-        setRulerEnable(panoRulerPlugin.state.enable)
-    }
+      await panoRulerPlugin.load(
+        panoRulerData.pano_ruler_data.roomInfo,
+        panoRulerData.pano_ruler_data.roomRules,
+        // {
+        //   distanceText: (distance) => `约 ${distance.toFixed(1)}米`,
+        // },
+      )
 
-    if (fiveModelReadyState !== 'Loaded') return null
-    return (
-        <Box>
-            <Paper sx={{ position: 'fixed', top: '10px', right: '10px', backgroundColor: 'transparent' }}>
-                <Button onClick={handleRulerEnable}>
-                    {rulerEnable ? OPERATE_OPTIONS.CLOSE : OPERATE_OPTIONS.OPEN}
-                </Button>
-            </Paper>
-        </Box>
-    )
+      panoRulerPlugin.enable()
+      setRulerEnable(panoRulerPlugin.state.enable)
+    },
+    [panoRulerData],
+  )
+
+  const handleRulerEnable = () => {
+    panoRulerPlugin[panoRulerPlugin.state.enable ? 'disable' : 'enable']()
+    setRulerEnable(panoRulerPlugin.state.enable)
+  }
+
+  if (fiveModelReadyState !== 'Loaded') return null
+  return (
+    <Box>
+      <Paper
+        sx={{
+          position: 'fixed',
+          top: '10px',
+          right: '10px',
+          backgroundColor: 'transparent',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '10px 0',
+        }}
+      >
+        <Button variant="contained" onClick={handleRulerEnable}>
+          {rulerEnable ? OPERATE_OPTIONS.CLOSE : OPERATE_OPTIONS.OPEN}
+        </Button>
+        <Button variant="contained" onClick={toggleUnit}>
+          切换单位
+        </Button>
+      </Paper>
+    </Box>
+  )
 }
 
 export default PanoRulerPluginUsage
