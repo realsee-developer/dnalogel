8aee78f0fb55b8566cc855894769d6be493ab47f
jiasonglin <jiasonglinfe@foxmail.com>
2023-09-06 15:43:32 +0800

    feat(pano-compass-plugin): 支持配置 logoUrl

--- src/PanoCompassPlugin/App.tsx
+++ src/PanoCompassPlugin/App.tsx
@@ -5,50 +5,48 @@ import { useWindowDimensions } from './useWindowDimensions'
 import { Box } from '@mui/material'
 import PanoCompassPluginUse from './PanoCompassPluginUse'
 import { parseWork } from '@realsee/five'
-import useFetchDatas, { DATA_TYPES } from "../utils/useFetchDatas";
+import useFetchDatas, { DATA_TYPES } from '../utils/useFetchDatas'
 
 const FiveProvider = createFiveProvider({
-    imageOptions: { size: 512 }, // 图片默认分辨率
-    textureOptions: { size: 512 }, // 贴图默认分辨率
-    onlyRenderIfNeeds: true,
-    plugins: [
-        [
-            PanoCompassPlugin,
-            'panoCompassPlugin',
-            {
-                // staticPrefix: 'a'
-                // compassImageUrl: ''
-            },
-        ],
+  imageOptions: { size: 512 }, // 图片默认分辨率
+  textureOptions: { size: 512 }, // 贴图默认分辨率
+  onlyRenderIfNeeds: true,
+  plugins: [
+    [
+      PanoCompassPlugin,
+      'panoCompassPlugin',
+      {
+        logoURL: '//vr-image-4.realsee-cdn.cn/release/web/logo.7febb17f.png',
+        // staticPrefix: 'a'
+        // compassImageUrl: ''
+      },
     ],
+  ],
 })
 
 const App: React.FC = () => {
-    const size = useWindowDimensions()
-    const work = useFetchDatas(DATA_TYPES.WORK)
+  const size = useWindowDimensions()
+  const work = useFetchDatas(DATA_TYPES.WORK)
 
-    return (
-        work && (
-            <FiveProvider
-                initialWork={parseWork(work)}
-                ref={(ref) => Object.assign(window, { $five: ref?.five })}
-            >
-                <FiveCanvas {...size} />
-                <Box
-                    className="plugin-full-screen-container"
-                    sx={{
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        width: '100%',
-                        height: '100%',
-                        pointerEvents: 'none',
-                    }}
-                />
-                <PanoCompassPluginUse />
-            </FiveProvider>
-        )
+  return (
+    work && (
+      <FiveProvider initialWork={parseWork(work)} ref={(ref) => Object.assign(window, { $five: ref?.five })}>
+        <FiveCanvas {...size} />
+        <Box
+          className="plugin-full-screen-container"
+          sx={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            pointerEvents: 'none',
+          }}
+        />
+        <PanoCompassPluginUse />
+      </FiveProvider>
     )
+  )
 }
 
 export default App
