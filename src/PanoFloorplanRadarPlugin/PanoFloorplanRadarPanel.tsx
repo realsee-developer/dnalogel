ef9301cb888ada0161135cd3b9c378bff8469528
jiasonglin <jiasonglinfe@foxmail.com>
2023-10-13 18:38:12 +0800

    feat(floorplan-radar): 雷达图支持配置缺失楼层展位图

--- src/PanoFloorplanRadarPlugin/PanoFloorplanRadarPanel.tsx
+++ src/PanoFloorplanRadarPlugin/PanoFloorplanRadarPanel.tsx
@@ -1,61 +1,55 @@
-import * as React from "react";
-import {unsafe__useFiveInstance, useFiveState} from "@realsee/five/react";
+import * as React from 'react'
+import { unsafe__useFiveInstance, useFiveState } from '@realsee/five/react'
 import { Box } from '@mui/material'
-import { Five } from "@realsee/five";
-import useFetchDatas, { DATA_TYPES } from "../utils/useFetchDatas";
-import type {
-    PanoFloorplanRadarPlugin,
-    FloorplanServerData
-} from '@realsee/dnalogel'
+import { Five } from '@realsee/five'
+import useFetchDatas, { DATA_TYPES } from '../utils/useFetchDatas'
+import type { FloorplanServerData } from '@realsee/dnalogel'
 
 const PanoFloorplanRadarPanel: React.FC = () => {
-    const [fiveState, setFiveState] = useFiveState();
-    const floorplanServerData: FloorplanServerData = useFetchDatas(DATA_TYPES.FLOOR_PLAN_SERVER_PLUGIN_DATA)
-    const panoFloorplanRadarPanelRef = React.useRef<HTMLDivElement>(null)
-    const [visible, setVisible] = React.useState<boolean>(false)
-    const five = unsafe__useFiveInstance()
-    const panoFloorplanRadarPlugin = five.plugins.panoFloorplanRadarPlugin
-    panoFloorplanRadarPlugin.changeConfigs({ hoverEnable: true })
+  const [fiveState, setFiveState] = useFiveState()
+  const floorplanServerData: FloorplanServerData = useFetchDatas(DATA_TYPES.FLOOR_PLAN_SERVER_PLUGIN_DATA)
+  const panoFloorplanRadarPanelRef = React.useRef<HTMLDivElement>(null)
+  const [visible, setVisible] = React.useState<boolean>(false)
+  const five = unsafe__useFiveInstance()
+  const panoFloorplanRadarPlugin = five.plugins.panoFloorplanRadarPlugin
+  panoFloorplanRadarPlugin.changeConfigs({ hoverEnable: true })
 
+  React.useEffect(() => {
+    if (!panoFloorplanRadarPanelRef.current || fiveState.mode !== Five.Mode.Panorama) return
+    panoFloorplanRadarPlugin.appendTo(panoFloorplanRadarPanelRef.current)
+  }, [])
 
-    React.useEffect(() => {
-        if (!panoFloorplanRadarPanelRef.current || fiveState.mode !== Five.Mode.Panorama) return
-        panoFloorplanRadarPlugin.appendTo(panoFloorplanRadarPanelRef.current)
-    }, [])
+  React.useEffect(() => {
+    if (!floorplanServerData || JSON.stringify(floorplanServerData) === '{}') return
+    panoFloorplanRadarPlugin.load(floorplanServerData)
+  }, [floorplanServerData])
 
-    React.useEffect(() => {
-        if(!floorplanServerData || JSON.stringify(floorplanServerData) === '{}') return
-        panoFloorplanRadarPlugin.load(floorplanServerData)
-    }, [floorplanServerData])
+  React.useEffect(() => {
+    if (fiveState.mode === Five.Mode.Panorama) {
+      setVisible(true)
+    } else {
+      setVisible(false)
+    }
+  }, [fiveState.mode])
 
-    React.useEffect(() => {
-        if (fiveState.mode === Five.Mode.Panorama) {
-            setVisible(true)
-        }
-        else {
-            setVisible(false)
-        }
-
-    }, [fiveState.mode])
-
-    return (
-        <Box
-            onClick={() => setFiveState({ mode: Five.Mode.Floorplan })}
-            sx={{
-                display: `${visible ? 'flex' : 'none'}`,
-                justifyContent: 'center',
-                alignItems: 'center',
-                position: "absolute",
-                top: '60px',
-                right: '20px',
-                padding: '10px',
-                width: '90px',
-                height: '120px',
-                backgroundColor: 'rgba(0, 0, 0, .2)',
-            }}
-            ref={panoFloorplanRadarPanelRef}
-        />
-    )
+  return (
+    <Box
+      onClick={() => setFiveState({ mode: Five.Mode.Floorplan })}
+      sx={{
+        display: `${visible ? 'flex' : 'none'}`,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        top: '60px',
+        right: '20px',
+        padding: '10px',
+        width: '90px',
+        height: '120px',
+        backgroundColor: 'rgba(0, 0, 0, .2)',
+      }}
+      ref={panoFloorplanRadarPanelRef}
+    />
+  )
 }
 
 export default PanoFloorplanRadarPanel
