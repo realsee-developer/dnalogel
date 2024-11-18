4c958743bf28caa589a1dee4331024499d046d95
lihaozheng001 <lihaozheng001@realsee.com>
2024-11-18 20:44:03 +0800

    fix: build

--- src/ModelMakerPlugin/mocks/data.ts
+++ src/ModelMakerPlugin/mocks/data.ts
@@ -14,7 +14,6 @@ export const data: ModelMakerPluginType.ServerData = {
           [-4.322683971372346, -0.012037871935272133, 0.7562943264963584],
         ],
         color: '#ff00ff',
-        panoIndex: 2,
         opacity: 0.4,
       },
     },
