47b9da7683e91737ce3674de03bd854cfe789d8f
lihaozheng001 <lihaozheng001@realsee.com>
2024-11-28 20:22:18 +0800

    feat: 3.60.0

--- src/PanoMeasurePlugin/App.tsx
+++ src/PanoMeasurePlugin/App.tsx
@@ -14,6 +14,7 @@ const pcConfig: Parameters<typeof PanoMeasurePlugin>[1] = {
     useNewUI: true,
   },
   editParams: {
+    pointSelectorMode: 'cursor',
     allowMeasureType: ['area'],
     autoEndConfig: { line: 3 },
   },
