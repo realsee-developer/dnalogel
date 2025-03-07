e1a220811de54cc33f061d0f2f38becef0300b1d
李成杰(23089611) <lichengjie003@realsee.com>
2025-03-07 08:26:23 +0000

    feat(ModelMakerPlugin): 增强模型透明度控制和类型定义

--- src/ModelMakerPlugin/PluginUse.tsx
+++ src/ModelMakerPlugin/PluginUse.tsx
@@ -1,7 +1,7 @@
 import { unsafe__useFiveInstance } from '@realsee/five/react'
-import { Paper, ButtonGroup, Button } from '@mui/material'
-import React from 'react'
+import { Paper, ButtonGroup, Button, Slider, Typography, Box } from '@mui/material'
 import { type ModelMakerController } from '@realsee/dnalogel/dist/ModelMakerPlugin/typings'
+import React from 'react'
 import { data } from './mocks/data'
 import { data2 } from './mocks/data2'
 import { data3 } from './mocks/data3'
@@ -10,6 +10,7 @@ import { FiveModeSwitcher } from '../components/FiveModeSwitcher'
 const PluginUse = () => {
   const five = unsafe__useFiveInstance()
   const pluginInstance = five.plugins.modelMakerPlugin as ModelMakerController
+  const [opacityPercent, setOpacityPercent] = React.useState(1) // 默认为100%
 
   React.useEffect(() => {
     pluginInstance.load(data)
@@ -18,10 +19,16 @@ const PluginUse = () => {
     }
   }, [five])
 
+  // 更新所有模型的透明度百分比
+  const updateOpacity = (percent: number) => {
+    setOpacityPercent(percent)
+    pluginInstance.setModelsOpacity(percent)
+  }
+
   return (
     <>
       <FiveModeSwitcher modeList={['Panorama', 'Mapview', 'Floorplan']} />
-      <Paper sx={{ position: 'fixed', top: '10px', right: '10px', backgroundColor: 'transparent' }}>
+      <Paper sx={{ position: 'fixed', top: '10px', right: '10px', backgroundColor: 'transparent', padding: '10px' }}>
         <ButtonGroup size="large" aria-label="large button group" orientation="vertical">
           <Button onClick={() => pluginInstance.load(data)}>data1</Button>
           <Button onClick={() => pluginInstance.load(data2)}>data2</Button>
@@ -34,6 +41,20 @@ const PluginUse = () => {
           <Button onClick={() => pluginInstance?.enable()}>enable</Button>
           <Button onClick={() => pluginInstance.load(data)}>load</Button>
         </ButtonGroup>
+        
+        <Box sx={{ width: '100%', mt: 2 }}>
+          <Typography gutterBottom>透明度百分比 ({Math.round(opacityPercent * 100)}%)</Typography>
+          <Slider
+            value={opacityPercent}
+            min={0}
+            max={1}
+            step={0.01}
+            onChange={(_, newValue) => updateOpacity(newValue as number)}
+            valueLabelDisplay="auto"
+            valueLabelFormat={(value) => `${Math.round(value * 100)}%`}
+            aria-labelledby="opacity-slider"
+          />
+        </Box>
       </Paper>
     </>
   )
